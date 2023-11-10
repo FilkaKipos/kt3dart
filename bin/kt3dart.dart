c06d6fd5e5e977c43bd2aa2b0f3716dbaa1858f6
ghostbang <rezidreta@gmail.com>
2023-11-10 18:23:23 +0500

    commit

---
+++ bin/kt3dart.dart
@@ -0,0 +1,6 @@
+
+import 'package:kt3dart/kt3dart.dart' as kt3dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kt3dart.calculate()}!');
+}
---
+++ test/kt3dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:kt3dart/kt3dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
